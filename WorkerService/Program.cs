30c955a1c8280af0ad08dea65196041d6b22d147
Waleed Ahmed <waleed.wit@gmail.com>
2022-09-25 08:36:46 +0300

    Add project files.

---
+++ WorkerService/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkerService;
+
+Console.WriteLine("Hello, World!");
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+await host.RunAsync();
